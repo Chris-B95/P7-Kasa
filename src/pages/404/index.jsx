51094f8b89f0321a76d1fa88d8a1de30aabbfd39
Chris-B95 <t-o-f@hotmail.fr>
2022-10-24 15:28:51 +0200

    404 added

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom'
+import "../../styles/pages/_404.scss"
+
 function PageNotFound() {
-    return <div>404</div>
+    return (<div className='kasa-404'>
+        <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <nav className='kasa-404__nav'><Link to="/">Retourner sur la page d'accueil</Link></nav>
+    </div>)
 }
 
 export default PageNotFound
